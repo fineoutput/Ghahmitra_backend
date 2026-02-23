b01c34f0a71496c1054d05551433a733134120af
Ayush <ayushrs7023@gmail.com>
2026-02-23 12:50:12 +0530

    //

--- resources/views/frontend/common/header.blade.php
+++ resources/views/frontend/common/header.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Tours Dekho</title>
+  <title>Grah Mitra</title>
   <!-- Bootstrap CSS -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
